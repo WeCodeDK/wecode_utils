8d6cbfe98414f18c9b570cbe7fd2bef6716f5830
Tobias Wecode <tobiaswecode@m1.local>
2022-11-24 16:25:23 +0100

    .

--- install_commands.sh
+++ install_commands.sh
@@ -2,9 +2,7 @@
 
 echo -e "\n\033[0;33mInfo: \033[0;37mInstalling cphp.."
 
-url="https://raw.githubusercontent.com/WeCodeDK/wecode_utils/main/commands/change_php.sh"
-
-curl -s "$url" > /usr/local/bin/cphp
+curl -s https://raw.githubusercontent.com/WeCodeDK/wecode_utils/main/commands/change_php.sh > /usr/local/bin/cphp
 sudo chmod +x /usr/local/bin/cphp
 # if the last two commands fail, the script will exit
 if [ $? -ne 0 ]; then
