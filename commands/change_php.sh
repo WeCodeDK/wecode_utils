11016e7e0ed4497212783bd100fca240e7691a2e
Tobias Wecode <tobiaswecode@m1.local>
2022-11-24 17:53:36 +0100

    Add description to cphp command

--- commands/change_php.sh
+++ commands/change_php.sh
@@ -6,6 +6,9 @@ Example: cphp 8.1
 
 Available versions:
 $(brew search php | grep php@ | sed 's/php@//g')
+
+Description:
+This script will change the php version used by the command line.
 "
 
 if [ "$1" == "" ]; then
